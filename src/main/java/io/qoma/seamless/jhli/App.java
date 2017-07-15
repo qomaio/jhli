a9b31ee0bf7f05028dc7bd3a66f10805e83c95a6
Kevin R. Keane <krkeane@qoma.io>
2017-07-15 20:46:11 +0000

    Format hello world.

--- src/main/java/io/qoma/seamless/jhli/App.java
+++ src/main/java/io/qoma/seamless/jhli/App.java
@@ -13,28 +13,27 @@ public class App {
 
 		// Initialize the C HLI
 		cfmini(status);
-		System.out.printf("cfmini(status) status = %d\n", status[0]);
+		System.out.printf("--\n  cfmini(status) status = %d\n", status[0]);
 		if (status[0] != HSUCC)
 			return;
 
 		// get machine info
-		String os = String.format("%s %s", System.getProperty("os.name"), System.getProperty("os.version"));
 		String vm = String.format("%s %s", System.getProperty("java.runtime.name"),
 				System.getProperty("java.runtime.version"));
 
 		// Get the C HLI version number
 		cfmver(status, ver);
-		System.out.printf("cfmver(status,ver) status = %d, ver = %.5f\n\n", status[0], ver[0]);
+		System.out.printf("  cfmver(status,ver) status = %d, ver = %.5f\n\n", status[0], ver[0]);
 		if (status[0] != HSUCC)
 			return;
 
 		// say HELLO !
-		System.out.printf("FIS MarketMap Analytic Platform\n" + "\tHello World from Java!\n");
-		System.out.printf("\tFAME HLI %.5f\n\t%s\n\t%s\n\n", ver[0], vm, os);
+		System.out.printf("\tFIS MarketMap Analytic Platform\n" + "\tHello World from Java!\n");
+		System.out.printf("\tFAME HLI %.5f\n\t%s\n\n", ver[0], vm);
 
 		// Finish the C HLI
 		cfmfin(status);
-		System.out.printf("cfmfin(status) status = %d\n\n", status[0]);
+		System.out.printf("  cfmfin(status) status = %d\n--\n", status[0]);
 		if (status[0] != HSUCC)
 			return;
 	}
